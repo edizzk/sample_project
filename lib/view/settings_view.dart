d52ec4984fc9f68c10c260465d591e50c70166c1
edizzk <edizmetekurt@gmail.com>
2022-06-28 08:33:49 +0300

    create settings page

---
+++ lib/view/settings_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:get/get.dart';
+
+
+class SettingsView extends StatelessWidget {
+  const SettingsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('project_title'.tr)),
+      body: Column(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: Text("change_language".tr),
+            onTap: () {
+
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
