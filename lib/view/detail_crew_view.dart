2e54c5ac2a8bc6b9f17b6a790153fe7bd08347ad
edizzk <edizmetekurt@gmail.com>
2022-06-28 08:34:07 +0300

    create detail crew page

---
+++ lib/view/detail_crew_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DetailCrewView extends StatelessWidget {
+  const DetailCrewView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Detail Crew View"));
+  }
+
+}
