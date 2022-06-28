cbf9f4460f73b8de51f2d2d521b21415befc456c
edizzk <edizmetekurt@gmail.com>
2022-06-28 08:31:30 +0300

    create localization field and some values

---
+++ lib/service/localization.dart
@@ -0,0 +1,36 @@
+// ignore: depend_on_referenced_packages
+import 'package:get/get.dart';
+
+class Localization extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+    'en_US': {
+      'project_title': 'Sample Project',
+      'contracts_menu': 'Contracts',
+      'crm_menu': 'CRM',
+      'vessel_menu': 'Vessel',
+      'crew_menu': 'Crew',
+      'marine_menu': 'Marine',
+      'settings_menu': 'Settings',
+      'change_language': 'Change Language',
+      'choose_language': 'Choose Language',
+      'ok': 'Okey',
+      'english': 'English',
+      'turkish': 'Turkish',
+    },
+    'tr_TR': {
+      'project_title': 'Örnek Proje',
+      'contracts_menu': 'Contracts',
+      'crm_menu': 'CRM',
+      'vessel_menu': 'Vessel',
+      'crew_menu': 'Crew',
+      'marine_menu': 'Marine',
+      'settings_menu': 'Ayarlar',
+      'change_language': 'Dili Değiştir',
+      'choose_language': 'Dili Seç',
+      'ok': 'Tamam',
+      'english': 'İngilizce',
+      'turkish': 'Türkçe',
+    }
+  };
+}
